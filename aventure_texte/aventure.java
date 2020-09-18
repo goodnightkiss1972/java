5910d11e327612edd128d617c9afadbeb1fea46d
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2020-09-18 12:32:07 +0200

    premiere version de l'aventure texte

---
+++ aventure_texte/aventure.java
@@ -0,0 +1,8 @@
+public class aventure {
+
+	public static void main(String[] args) {
+		chaineAugmentee s1 = new chaineAugmentee("ma chaine de test, et maintenant un complement d'Aspremont.");
+		s1.afficheMots();
+	}
+
+}
---
+++ aventure_texte/chaineAugmentee.java
@@ -0,0 +1,36 @@
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
+ 
+public class chaineAugmentee {
+	String chaineSaisie;
+	List<String> listeMots = new ArrayList<String>();
+	int nbMots = 0;
+
+	// constructeur
+	public chaineAugmentee(String saisie) {
+		chaineSaisie = saisie;
+		chaineSaisie = chaineSaisie.replace(",", " ");
+		chaineSaisie = chaineSaisie.replace(".", " ");
+		chaineSaisie = chaineSaisie.replace(";", " ");
+		chaineSaisie = chaineSaisie.replace("'", " ");
+		decoupeMots();
+	}
+
+	public void decoupeMots() {
+		StringTokenizer st = new StringTokenizer(chaineSaisie, " ", false);
+		//System.out.println("Les mots: " + st.countTokens());
+		while (st.hasMoreTokens()) {
+			//System.out.println(st.nextToken());
+			listeMots.add(st.nextToken());
+			nbMots++;
+		}
+	}
+
+	public void afficheMots() {
+		for (String mot: listeMots) {
+			System.out.format("#" + mot + "#");
+		}
+		System.out.format("%n");
+	}
+}
