841c4a5ad101d103d419b7c1254bf364129faa14
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2020-10-16 17:01:21 +0200

    ajout de la permutation dans un tableau d'entier

--- tableau.java
+++ tableau.java
@@ -3,7 +3,8 @@ public class tableau {
     public static void main (String args[]) {
 	int[] tab1 = {4, 5, 1, 2, 3, 4, 0, -3, 2};
 	afficheTableau(tab1);
-
+	tab1 = permuteTableauParIndice(tab1, 2, 5);
+	afficheTableau(tab1);
     }
 
     static int[] triTableauInteger(int[] tab) {
@@ -32,6 +33,18 @@ public class tableau {
 	    }
 	    System.out.print(" ]\n");
 	}
-
     }
+
+    static int[] permuteTableauParIndice(int[] tab, int indice1, int indice2) {
+	if ((tab.length < 2) || (indice1 == indice2) || (indice1 >= tab.length) || (indice2 >= tab.length)) {
+		// Impossible de permuter, le tableau n'a pas au moins deux elements.
+		// ou bien les indices sont egaux et alors il n'y a rien a faire.
+		return tab;
+	}
+	int temp = tab[indice2];
+	tab[indice2] = tab[indice1];
+	tab[indice1] = temp;
+	return tab;
+    }
+
 }
