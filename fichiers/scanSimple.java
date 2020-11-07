febc87159f29e3e14132bdf272d1b10eb4a79614
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2020-11-07 10:13:37 +0100

    jeu de fichiers

---
+++ fichiers/scanSimple.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class scanSimple
+{
+  public static void main(String args[])
+  {
+    try
+    {
+      // Le fichier d'entrée
+      FileInputStream file;
+      if (args.length == 1) {
+         file = new FileInputStream(args[0]);
+      }
+      else {
+        file = new FileInputStream("data1.txt");
+      }
+   
+      Scanner scanner = new Scanner(file);  
+      
+      //renvoie true s'il y a une autre ligne à lire
+      while(scanner.hasNextLine())
+      {
+        System.out.println(scanner.nextLine());
+      }
+      scanner.close();    
+    }
+    catch(IOException e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
